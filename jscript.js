b86ce210edfabd62a1a8135ef8e2dd771dd62de3
Eduardo Valladares <edvl39@gmail.com>
2023-08-29 11:10:30 -0600

    Add files via upload

--- jscript.js
+++ jscript.js
@@ -1,20 +1,17 @@
-const container = document.querySelector('.image-container');
-const images = container.querySelectorAll('img');
-const arrows = container.querySelectorAll('.arrow');
-let currentImageIndex = 0;
-
-arrows.forEach((arrow) => {
-  arrow.addEventListener('click', (event) => {
-    event.preventDefault();
-    const direction = arrow.classList.contains('right') ? 1 : -1;
-    currentImageIndex += direction;
-    if (currentImageIndex < 0) {
-      currentImageIndex = images.length - 1;
-    } else if (currentImageIndex >= images.length) {
-      currentImageIndex = 0;
-    }
-    container.style.transform = `translateX(-${currentImageIndex * 100}%)`;
-  });
-});
-
-
+alert("Promocion de fin de mes ! Descuentos en rentas aniticipadas par el mes de Septiembre !");
+function hideElement() {
+  var image = document.getElementById('customModal');
+  if (image.style.display == "block") {
+    image.style.display = "none";
+  } else {
+    image.style.display ="none";
+  }
+}
+function showElement() {
+  var images = document.getElementById('customModal');
+  if (images.style.display == "none") {
+    images.style.display = "block";
+  } else {
+    images.style.display ="none";
+  }
+}

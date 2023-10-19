12bb5b828dbb5f4b97106ba9d2ac82b9ef72fc10
Eduardo Valladares <edvl39@gmail.com>
2023-10-19 13:03:05 -0500

    Update jscript.js
    Remove promo pop up

--- jscript.js
+++ jscript.js
@@ -1,17 +1,3 @@
-alert("Promocion de fin de mes ! Descuentos en rentas aniticipadas par el mes de Septiembre !");
-function hideElement() {
-  var image = document.getElementById('customModal');
-  if (image.style.display == "block") {
-    image.style.display = "none";
-  } else {
-    image.style.display ="none";
-  }
-}
-function showElement() {
-  var images = document.getElementById('customModal');
-  if (images.style.display == "none") {
-    images.style.display = "block";
-  } else {
-    images.style.display ="none";
-  }
+
+    
 }
